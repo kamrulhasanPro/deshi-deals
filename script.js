6bd644624c40a54226270e4a9e17d6e0240a350e
kamrulmiazi <kamrul116522@gmail.com>
2025-08-30 01:04:50 +0600

    basic complete

--- script.js
+++ script.js
@@ -38,18 +38,52 @@ for(let productBtn of productsBtn){
    
     productBtn.addEventListener('click', function productFunction(event){
         const price = Number(event.target.parentNode.parentNode.children[2].children[0].innerText);
-        console.log(price);
-        console.log(makeId('total-price').innerText);
+        // console.log(price);
+        // console.log(makeId('total-price').innerText);
 
         
         let totalPrice = numberString('total-price')
-        console.log(totalPrice, price);
+        // console.log(totalPrice, price);
         let totalPriceInner = totalPrice + price;
         innerId('total-price', totalPriceInner) 
         
         let discountPrice = totalPriceInner -  numberString('discount-text');
         innerId('total', discountPrice)
 
+
+        const title = event.target.parentNode.parentNode.children[1].innerText;
+
+        const productPrice = event.target.parentNode.parentNode.children[2].children[0].innerText
+        
+        const imgSrc = event.target.parentNode.parentNode.parentNode.children[0].children[0].src
+        
+        const list = document.createElement('li');
+        list.innerHTML = `
+            <div class="flex items-center justify-between bg-[#F4F1F1] rounded-md py-2 mb-4">
+                  <img src="${imgSrc}" class="w-12" alt="">
+                  <div>
+                    <h5>${title}</h5>
+                    <p>${productPrice} Tk</p>
+                  </div>
+            </div>
+        `
+
+        makeId('product-cart').appendChild(list)
     })
 }
 
+const promoCopy = makeId('copy-promo')
+promoCopy.addEventListener('click', function(){
+    const text = promoCopy.innerText
+    navigator.clipboard.writeText(text)
+})
+
+makeId('purchase').addEventListener('click', ()=>{
+    if(numberString('total-price') > 0){
+        alert('your purchase complete')
+    }
+    makeId('total-price').innerText = '00'
+    makeId('product-cart').innerHTML = ''
+    makeId('discount-text').innerText = '00'
+    makeId('total').innerText = '00'
+})
